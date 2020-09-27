480f14ddec222c62b8af06a81a27fafba0fb8db2
Emmanuel Durand <shernua@gmail.com>
2020-09-27 12:15:07 -0500

    info to put on md

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const inquirer = require("inquirer");
+const axios = require("axios");
 const fs = require("fs");
-const util = require("util");
+const path = require('path');
 
-const writeFileAsync = util.promisify(fs.writeFile);
 
 function promptUser() {
     return inquirer.prompt([{
@@ -65,37 +65,35 @@ function promptUser() {
     ]);
 }
 
-function generateHTML(answers) {
-    return `
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-  <title>Document</title>
-</head>
-<body>
-  <div class="jumbotron jumbotron-fluid">
-  <div class="container">
-    <h1 class="display-4">Hi! My name is ${answers.name}</h1>
-    <p class="lead">I am from ${answers.location}.</p>
-    <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
-    <ul class="list-group">
-      <li class="list-group-item">My GitHub username is ${answers.github}</li>
-      <li class="list-group-item">LinkedIn: ${answers.linkedin}</li>
-    </ul>
-  </div>
-</div>
-</body>
-</html>`;
-}
+var result = (`
+  # ${answer.title} 
+  ${answer.description}
+  \n* [Installation](#Installation)
+  \n* [Instructions](#Instructions)
+  \n* [License](#License)
+  \n* [Contributors](#Contributors)
+  \n* [Author](#Author)
+  \n* [Tests](#Tests)
+  ## Installation
+  ${answer.installationInstructions}
+  ## Instructions
+  ${answer.usageInformation}
+  \`\`\`
+  ${answer.testInstructions}
+  \`\`\`
+  ## License 
+  This project is licensed under the ${answer.licenseList}
+  ## Author 
+  \n**${answer.gitHubName}**
+  \nEmail: ${answer.emailAddress}
+  `)
+
 
 promptUser()
     .then(function(answers) {
-        const html = generateHTML(answers);
+        const result = generateMd(answers);
 
-        return writeFileAsync("index.html", html);
+        return writeResult("index.html", html);
     })
     .then(function() {
         console.log("Successfully wrote to index.html");
@@ -103,3 +101,6 @@ promptUser()
     .catch(function(err) {
         console.log(err);
     });
+
+
+var writeResult = fs.writeFileSync(path.join(__dirname, '../GoodREADMeGenerator', 'readMe.md'), result)
